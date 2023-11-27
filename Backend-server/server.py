1623321b3279d6377e86623cbc483dd3daede159
SaifAlam <kaif0350@gmail.com>
2023-11-27 13:50:19 +0530

    frontend connection done

--- Backend-server/server.py
+++ Backend-server/server.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Route
 
 @app.route("/members")
 def members():
-    return {"members":["Member1","Member2","Member3"]}
+    return {"members": ["Member1","Member2","Member3"]}
+
+@app.route("/home")
+def home():
+    return render_template('home.html')
 
 
 if __name__ == "__main__":
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,50 +1,33 @@
-import './App.css'
-import { useState, useEffect } from 'react'
-import Home from './Components/Home/Home'
+import "./App.css";
+import { useState, useEffect } from "react";
+import Home from "./Components/Home/Home";
 
 function App() {
-
-  const [data,setData] = useState([])
+  const [data, setData] = useState([{}]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("/members");
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const data = await response.json();
+    const apiURL = "http://localhost:5000/members";
+    fetch(apiURL)
+      .then((res) => res.json())
+      .then((data) => {
         setData(data);
         console.log(data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-  
-    fetchData();
+      });
   }, []);
-  
 
   return (
     <>
-
-    <div>
-
-    {(typeof data.members === 'undefined') ? (
-      <p>Loading...</p>
-    ) : (
-      data.members.map((member, i) => (
-        <p key={i}>{member}</p>
-      ))
-
-    )}
-
-    </div>
-
+      <div>
+        {typeof data.members === "undefined" ? (
+          <p>Loading...</p>
+        ) : (
+          data.members.map((member, i) => <p key={i}>{member}</p>)
+        )}
+      </div>
 
       <Home />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
